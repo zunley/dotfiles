00529bea837b294a55b2dfe55457a11891fe3235
merore <merore256@outlook.com>
2023-08-14 10:12:32 +0800

    nvim: enable ctrl+w when toggleterm float

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -1,10 +1,10 @@
 -- Enable switching between terminal and other windows.
-function _G.set_terminal_keymaps()
-    local opts = {buffer = 0}
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- function _G.set_terminal_keymaps()
+--     local opts = {buffer = 0}
+--     vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+--     vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+-- end
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 require('toggleterm').setup{
     open_mapping = [[<c-\>]],
